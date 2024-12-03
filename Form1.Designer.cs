89a2c00fcacb2f5da2b59ce90e1b8ccb7430cbcc
Александр Костенко <uwumordochka2023@gmail.com>
2024-12-03 23:35:32 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace SkladSystemManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.кодDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tovarDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.categoryDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.statusDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.skadItemsTableBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.skladDataSet = new SkladSystemManager.skladDataSet();
+            this.skadItemsTableTableAdapter = new SkladSystemManager.skladDataSetTableAdapters.skadItemsTableTableAdapter();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.skadItemsTableBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.skladDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.кодDataGridViewTextBoxColumn,
+            this.tovarDataGridViewTextBoxColumn,
+            this.categoryDataGridViewTextBoxColumn,
+            this.statusDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.skadItemsTableBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(46, 28);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(935, 479);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // кодDataGridViewTextBoxColumn
+            // 
+            this.кодDataGridViewTextBoxColumn.DataPropertyName = "Код";
+            this.кодDataGridViewTextBoxColumn.HeaderText = "Код";
+            this.кодDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.кодDataGridViewTextBoxColumn.Name = "кодDataGridViewTextBoxColumn";
+            this.кодDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // tovarDataGridViewTextBoxColumn
+            // 
+            this.tovarDataGridViewTextBoxColumn.DataPropertyName = "tovar";
+            this.tovarDataGridViewTextBoxColumn.HeaderText = "tovar";
+            this.tovarDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.tovarDataGridViewTextBoxColumn.Name = "tovarDataGridViewTextBoxColumn";
+            this.tovarDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // categoryDataGridViewTextBoxColumn
+            // 
+            this.categoryDataGridViewTextBoxColumn.DataPropertyName = "category";
+            this.categoryDataGridViewTextBoxColumn.HeaderText = "category";
+            this.categoryDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.categoryDataGridViewTextBoxColumn.Name = "categoryDataGridViewTextBoxColumn";
+            this.categoryDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // statusDataGridViewTextBoxColumn
+            // 
+            this.statusDataGridViewTextBoxColumn.DataPropertyName = "status";
+            this.statusDataGridViewTextBoxColumn.HeaderText = "status";
+            this.statusDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.statusDataGridViewTextBoxColumn.Name = "statusDataGridViewTextBoxColumn";
+            this.statusDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // skadItemsTableBindingSource
+            // 
+            this.skadItemsTableBindingSource.DataMember = "skadItemsTable";
+            this.skadItemsTableBindingSource.DataSource = this.skladDataSet;
+            // 
+            // skladDataSet
+            // 
+            this.skladDataSet.DataSetName = "skladDataSet";
+            this.skladDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // skadItemsTableTableAdapter
+            // 
+            this.skadItemsTableTableAdapter.ClearBeforeFill = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(863, 539);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 56);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Перейти к следующему этапу";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Info;
+            this.label1.Location = new System.Drawing.Point(48, 539);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Процесс:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label2.Location = new System.Drawing.Point(48, 579);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Местоположение:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(686, 539);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 56);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Отправить запрос \r\n";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label3.Location = new System.Drawing.Point(174, 579);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Местоположение:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.Info;
+            this.label4.Location = new System.Drawing.Point(120, 539);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Процесс:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.Info;
+            this.label5.Location = new System.Drawing.Point(567, 579);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 16);
+            this.label5.TabIndex = 7;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.Info;
+            this.label6.Location = new System.Drawing.Point(450, 579);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 16);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Время ответа:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1003, 653);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.skadItemsTableBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.skladDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private skladDataSet skladDataSet;
+        private System.Windows.Forms.BindingSource skadItemsTableBindingSource;
+        private skladDataSetTableAdapters.skadItemsTableTableAdapter skadItemsTableTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn кодDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tovarDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn categoryDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn statusDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SkladSystemManager
+{
+    public partial class Form1 : Form
+    {
+        public static string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=sklad.accdb";
+        private OleDbConnection myConnection;
+        public string procesNow, placeNow;
+        public int page=0;
+        public bool acces = false;
+        public string[] processes= {"Контроль качества","Присвоение уникального кода","Сортировка","Сбор заказа","Упаковка", "Отгрузка" ,"Доставка"};
+        public string[] places = {"Зона предварительного хранения","Зона контроля качества","Сортировочный конвеер","Зона сбоки и упаковки","Зона отгрузки","Товар за пределами склада"};
+        public Form1()
+        {
+            InitializeComponent();
+            myConnection = new OleDbConnection(connectionString);
+            myConnection.Open();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "skladDataSet.skadItemsTable". При необходимости она может быть перемещена или удалена.
+            this.skadItemsTableTableAdapter.Fill(this.skladDataSet.skadItemsTable);
+            NewMaterial();
+            procesNow = "Отгрузка";
+            placeNow = places[0];
+            label4.Text = " " + procesNow;
+            label3.Text = " " + placeNow;
+            button1.Enabled = acces;
+        }
+        public void PageMananger(int page)
+        {
+            
+                procesNow = processes[page];
+                switch (page)
+                {
+                    case 0:
+                        placeNow = places[0];
+                        break;
+                    case 1:
+                        placeNow = places[1];
+                        break;
+                    case 2:
+                        placeNow = places[1];
+                        string str = RandomCode();
+                        string query1 = "UPDATE skadItemsTable SET tovar =  " + "('" + str + "')";
+                        OleDbCommand command1 = new OleDbCommand(query1, myConnection);
+                        command1.ExecuteNonQuery();
+                        break;
+                    case 3:
+                        placeNow = places[2];
+                        break;
+                    case 4:
+                        placeNow = places[3];
+                        break;
+                    case 5:
+                        placeNow = places[3];
+                        break;
+                    case 6:
+                        placeNow = places[4];
+                        break;
+                    case 7:
+                        placeNow = places[5];
+                        break;
+                }
+                string query = "UPDATE skadItemsTable SET status =  " + "('" + procesNow + "')";
+                OleDbCommand command = new OleDbCommand(query, myConnection);
+                command.ExecuteNonQuery();
+                label4.Text = " " + procesNow;
+                label3.Text = " " + placeNow;
+            
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            myConnection.Close();
+        }
+        public void NewMaterial()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                string categoryName = RandomCategory();
+                string status = "Отгрука";
+                string query = "INSERT INTO skadItemsTable (category,status)VALUES  " + "('" + categoryName + "','" + status + "')";
+                OleDbCommand command = new OleDbCommand(query, myConnection);
+                command.ExecuteNonQuery();
+            }
+        }
+        public string RandomCategory()
+        {
+            Random random = new Random();
+            int category = random.Next(0, 6);
+            string res = "";
+            switch (category)
+            {
+                case 0:
+                    res = "Продукты питания";
+                    break;
+                case 1:
+                    res = "Техника и гаджеты";
+                    break;
+                case 2:
+                    res = "Одежда и обувь";
+                    break;
+                case 3:
+                    res = "Бытовые приборы";
+                    break;
+                case 4:
+                    res = "Бытовая химия";
+                    break;
+                case 5:
+                    res = "Литература";
+                    break;
+                case 6:
+                    res = "Сувениры";
+                    break;
+
+            }
+            return res;
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            
+        }
+        public int i = 0;
+        public int a = 25;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            
+            i++;
+            if (i>25)
+            {
+                acces = true;
+                button2.Enabled = false;
+                button1.Enabled=acces;
+                i = 0;
+                timer1.Stop();
+            }
+            label5.Text = (a - i).ToString();
+        }
+        public string RandomCode()
+        { 
+            Random random = new Random();
+            Random random1 = new Random();
+            int one=random1.Next(0, 9);
+            int two= random1.Next(0, 9);
+            int three = random1.Next(0, 9);
+            int four = random1.Next(0, 9);
+            int five = random1.Next(1, 26);
+            int six = random1.Next(1, 26);
+            int seven = random1.Next(1, 26);
+            int eigth = random1.Next(1, 26);
+            int ninne = random1.Next(0, 9);
+            int ten = random1.Next(0, 9);
+            int eleven = random1.Next(0, 9);
+            int twelwe = random1.Next(0, 9);
+            string code= one.ToString()+ two.ToString() + three.ToString() + four.ToString() + Switcharnick(five).ToString() + Switcharnick(six).ToString() + Switcharnick(seven).ToString() + Switcharnick(eigth).ToString() + ninne.ToString() + ten.ToString() + eleven.ToString()+ twelwe.ToString();
+            return code;
+        }
+        public string Switcharnick(int chir)
+        {
+            string str="";
+            switch (chir)
+            { 
+                case 1:
+                    str = "A";
+                    break;
+                case 2:
+                    str = "B";
+                    break;
+                case 3:
+                    str = "C";
+                    break;
+                case 4:
+                    str = "D";
+                    break;
+                case 5:
+                    str = "E";
+                    break;
+                case 6:
+                    str = "F";
+                    break;
+                case 7:
+                    str = "G";
+                    break;
+                case 8:
+                    str = "H";
+                    break;
+                case 9:
+                    str = "I";
+                    break;
+                case 10:
+                    str = "J";
+                    break;
+                case 11:
+                    str = "K";
+                    break;
+                case 12:
+                    str = "L";
+                    break;
+                case 13:
+                    str = "M";
+                    break;
+                case 14:
+                    str = "N";
+                    break;
+                case 15:
+                    str = "O";
+                    break;
+                case 16:
+                    str = "P";
+                    break;
+                case 17:
+                    str = "R";
+                    break;
+                case 18:
+                    str = "S";
+                    break;
+                case 19:
+                    str = "T";
+                    break;
+                case 20:
+                    str = "U";
+                    break;
+                case 21:
+                    str = "V";
+                    break;
+                case 22:
+                    str = "W";
+                    break;
+                case 23:
+                    str = "X";
+                    break;
+                case 24:
+                    str = "Y";
+                    break;
+                case 25:
+                    str = "Z";
+                    break;
+                case 26:
+                    break;
+            }
+            return str;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (acces)
+            {
+                if (page == 6)
+                {
+                    button1.Enabled = false;
+                    button2.Enabled = false;
+                    MessageBox.Show("Весь процесс завершен");
+                }
+                else
+                {
+                    PageMananger(page);
+                    this.skadItemsTableTableAdapter.Fill(this.skladDataSet.skadItemsTable);
+                    label5.Text = page.ToString();
+                    page++;
+                    acces = false;
+                    button1.Enabled = false;
+                    button2.Enabled = true;
+                }
+            }
+        }
+    }
+}
